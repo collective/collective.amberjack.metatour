70f6050c13e061669f8b20388d30eea7cc85aaab
Vincent Fretin <vincentfretin@ecreall.com>
2009-08-12 07:50:41 +0000

    Use an adapter instead of a view
    git-svn-id: http://svn.plone.org/svn/collective/collective.amberjack.metatour/trunk@94512 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/amberjack/metatour/adapters.py
@@ -0,0 +1,44 @@
+from zope.component import adapts
+from zope.interface import implements
+
+from collective.amberjack.core.interfaces import ITourDefinition
+from collective.amberjack.metatour.interfaces import Iajtour
+
+class TourAdapter(object):
+    implements(ITourDefinition)
+    adapts(Iajtour)
+
+    def __init__(self, context):
+        self.context = context
+
+    def tourId(self):
+        """Return the tourId."""
+        return self.context.tourId
+
+    def title(self):
+        """Return the title."""
+        return self.context.title
+
+    def steps(self):
+        """Return a dict:
+        {'url': 'url',
+         'xpath': 'xpath expression',
+         'xcontent': 'xcontent',
+         'title': 'title',
+         'text': 'text',
+         'steps': ((description, idStep, selector, text), ...)}
+
+        """
+        output = []
+
+        for item in self.context.listFolderContents():
+            d = {'url':  item.getUrl(),
+                 'xpath': item.getXpath(),
+                 'xcontent': item.getXcontent(),
+                 'title': item.Title(),
+                 'text': item.getText(),
+                 'steps': item.getSteps()}
+            output.append(d)
+        
+        return output
+
